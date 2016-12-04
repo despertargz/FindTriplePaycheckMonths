c2b0478fdc152f08a6bcb77655f8521becce5172
Christopher <mev412@gmail.com>
2016-12-04 00:51:51 -0600

    cleaned up

--- FindTriplePaycheckMonths/Program.cs
+++ FindTriplePaycheckMonths/Program.cs
@@ -9,60 +9,20 @@ namespace FindTriplePaycheckMonths
 {
     class Program
     {
+        static Dictionary<int, int> PaychecksPerMonth = new Dictionary<int, int>();
+
         static void Main(string[] args)
         {
             Console.Write("Enter Any Paycheck Date: ");
             string payDateStr = Console.ReadLine();
-
             var payDate = DateTime.Parse(payDateStr);
-            var originalPayDate = payDate;
-            int startingYear = payDate.Year;
-
-            Dictionary<int, int> paychecksPerMonth = new Dictionary<int, int>();
-            paychecksPerMonth[payDate.Month] = 1;
-            
-            while (true)
-            {
-                payDate = payDate.Subtract(TimeSpan.FromDays(14));
-
-                if (payDate.Year != startingYear)
-                {
-                    break;
-                }
-                else
-                {
-                    if (!paychecksPerMonth.ContainsKey(payDate.Month))
-                    {
-                        paychecksPerMonth[payDate.Month] = 0;
-                    }
-
-                    paychecksPerMonth[payDate.Month]++;
-                }
-            }
-
-            payDate = originalPayDate;
-
-            while (true)
-            {
-                payDate = payDate.Add(TimeSpan.FromDays(14));
-
-                if (payDate.Year != startingYear)
-                {
-                    break;
-                }
-                else
-                {
-                    if (!paychecksPerMonth.ContainsKey(payDate.Month))
-                    {
-                        paychecksPerMonth[payDate.Month] = 0;
-                    }
-
-                    paychecksPerMonth[payDate.Month]++;
-                }
-            }
+            PaychecksPerMonth[payDate.Month] = 1;
+          
+            FindPaychecksPerMonth(payDate, -14);
+            FindPaychecksPerMonth(payDate, 14);
 
             Console.WriteLine();
-            foreach (var pair in paychecksPerMonth.OrderBy(o => o.Key).Where(o => o.Value > 2))
+            foreach (var pair in PaychecksPerMonth.OrderBy(o => o.Key).Where(o => o.Value > 2))
             {
                 string mon = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(pair.Key);
                 Console.WriteLine(mon);
@@ -71,6 +31,33 @@ namespace FindTriplePaycheckMonths
             Console.WriteLine();
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
+        }
+
+        static void FindPaychecksPerMonth(DateTime startingPayDate, int dayChange, bool print = false)
+        {
+            DateTime payDate = startingPayDate;
+
+            while (true)
+            {
+                payDate = payDate.AddDays(dayChange);
+
+                if (payDate.Year != startingPayDate.Year)
+                {
+                    break;
+                }
+
+                if (!PaychecksPerMonth.ContainsKey(payDate.Month))
+                {
+                    PaychecksPerMonth[payDate.Month] = 0;
+                }
+
+                PaychecksPerMonth[payDate.Month]++;
+
+                if (print)
+                {
+                    Console.WriteLine(payDate);
+                }
+            }
         }
     }
 }
